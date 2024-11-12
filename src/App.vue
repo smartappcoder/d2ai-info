1d96ac6acc8f0291ed4fbd1b129c2951845e032d
Dennis Kung <dennis.kung@cooltrax.com>
2024-11-13 00:38:08 +1000

    report example

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import Todos from './components/Todos.vue'
+import Reports from './components/Reports.vue'
 </script>
 
 <template>
   <main>
-    <Todos />
+    <Reports />
   </main>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import App from "./App.vue";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles';  // Global CSS has to be imported
+
 Amplify.configure(outputs);
 
-createApp(App).mount("#app");
+const vuetify = createVuetify({ 
+    components, 
+    directives 
+})
+
+createApp(App).use(vuetify).mount("#app")
